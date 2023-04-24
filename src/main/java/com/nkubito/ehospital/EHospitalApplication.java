64e32c481ea42bb71f089fc5289fcbb2da8d0668
nkbtemmy <nkubito@augmented-future.com>
2023-04-24 19:30:59 +0200

    backend setup

---
+++ src/main/java/com/nkubito/ehospital/EHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.nkubito.ehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nkubito/ehospital/EHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkubito.ehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
