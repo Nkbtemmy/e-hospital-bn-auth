54e12851cd01a457c5715a6e01e3f2ac1a0fe92b
nkbtemmy <nkubito@augmented-future.com>
2023-04-30 21:59:23 +0200

    getting all users

--- src/main/java/com/nkubito/ehospital/store/Users.java
+++ src/main/java/com/nkubito/ehospital/store/Users.java
@@ -3,7 +3,9 @@ package com.nkubito.ehospital.store;
 import com.nkubito.ehospital.models.User;
 import com.nkubito.ehospital.utils.BadRequest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public enum Users {
@@ -40,5 +42,14 @@ public enum Users {
             case Pharmacist -> users.keySet().stream().anyMatch(u -> u.equals(user.getPhoneNumber()));
             case Patient -> users.keySet().stream().anyMatch(u -> u.equals(user.getUsername()));
         };
+    }
+    public List<User> retreiveUser() {
+        List<User> userList = new ArrayList<>();
+        Map<String, User> userMap = Users.INSTANCE.getUsers();
+        for (Map.Entry<String, User> entry : userMap.entrySet()) {
+            User user = entry.getValue();
+            userList.add(user);
+        }
+        return userList;
     }
 }
